4a781d811f7a578d9466335c55a1ab184537a618
davidka <davidka@davidka.com>
2024-08-08 02:09:08 +0300

    Add CSS and JS files and update HTML to display results on the page

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function printSumOfArray(params) {
 
 function fizzBuzz() {
     let result = [];
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 0; i <= 100; i++) {
         if (i % 15 === 0) {
             result.push("FizzBuzz");
         } else if (i % 3 === 0) {
