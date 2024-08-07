5a033920e54bdce433887b06e4d2076d9599b07b
davidka <davidka@davidka.com>
2024-08-08 00:21:12 +0300

    Add Zero, fix fizzBuzz

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function printSumOfArray(params) {
 }
 
 function fizzBuzz() {
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 0; i <= 100; i++) {
         if (i % 15 === 0) {
             console.log("FizzBuzz");
         } else if (i % 3 === 0) {
